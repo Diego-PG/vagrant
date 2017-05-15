10be6f04c555bf17b669252d4df4857c568e7b29
Diego Prieto <diegoprieto5@gmail.com>
2017-05-15 12:11:11 +0200

    carpeta compartida en demo

--- demo/config.sh
+++ demo/config.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
-apt-get update
-apt-get install vim -y
 cp -r /home/vagrant/files/skel/* /etc/skel
 useradd -m -s /bin/bash -G admin diego
 echo diego:secreto | chpasswd
-cp /home/vagrant/files/vimrc /home/diego/.vimrc
 cp /home/vagrant/files/motd  /etc/motd.tail
+chown -R diego:diego /opt/compartida
+ln -s /opt/compartida/ /home/diego/
