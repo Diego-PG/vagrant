1e467333dc932889bccde02793dd3d9c8f717ed0
Diego Prieto <diegoprieto5@gmail.com>
2017-05-23 09:33:35 +0200

    Cambio motd

--- demo/config.sh
+++ demo/config.sh
@@ -3,6 +3,7 @@ cp -r /home/vagrant/files/skel/* /etc/skel
 useradd -m -s /bin/bash -G admin diego
 echo diego:secreto | chpasswd
 cp /home/vagrant/files/motd  /etc/motd.tail
+chmod -x /etc/update-motd.d/00-header /etc/update-motd.d/10-help-text /etc/update-motd.d/91-release-upgrade
 chown -R diego:diego /opt/compartida
 ln -s /opt/compartida/ /home/diego/
 tar -xvf /home/diego/compartida/archivo.tar -C /home/diego
